6d6d80042f052989855d55254c85edba67b63135
Bruno Perkovic <brunko10@gmail.com>
2022-05-17 15:17:21 +0200

    boje ikona navbara

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -8,24 +8,24 @@ import { ReactComponent as ProfileIcon } from "./navbar-icons/user.svg";
 const Navbar = () => {
   return (
     <div className="navbar">
-      <a className="navbar__item navbar--active">
+      <a className="navbar__item navbar__item--active">
         <span>Home</span>
-        <HomeIcon className="navbar__item--icon  navbar__item--active" />
+        <HomeIcon className="navbar__icon " />
       </a>
 
       <a className="navbar__item">
         <span>Search</span>
-        <SearchIcon className="navbar__item--icon" />
+        <SearchIcon className="navbar__icon" />
       </a>
 
       <a className="navbar__item">
         <span>Bookmark</span>
-        <BookmarkIcon className="navbar__item--icon" />
+        <BookmarkIcon className="navbar__icon" />
       </a>
 
       <a className="navbar__item">
         <span>Profile</span>
-        <ProfileIcon className="navbar__item--icon" />
+        <ProfileIcon className="navbar__icon" />
       </a>
     </div>
   );
