33c009879866798fc6de346422fea8b8e5d1989d
Bruno Perkovic <brunko10@gmail.com>
2022-05-17 13:59:58 +0200

    nesto moje

--- src/Pages/Overview.js
+++ src/Pages/Overview.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Settings from "../Settings/Settings";
-import NasSwiper from "../Swiper/NasSwiper";
-import NasNavbar from "../Navbar/Navbar";
+import Settings from "../Components/Settings/Settings";
+import NasSwiper from "../Components/Swiper/NasSwiper";
+import NasNavbar from "../Components/Navbar/Navbar";
 import "./Overview.scss";
 
 const Overview = () => {
