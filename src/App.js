891348e3be14394ff689786cd0ec48520dc86428
Bruno Perkovic <brunko10@gmail.com>
2022-05-19 12:45:33 +0200

    ne znan nista

--- src/App.js
+++ src/App.js
@@ -6,19 +6,6 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [weather, setWeather] = useState("");
-
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=3b73ba60020b3ca9b6ba259cf70a6931"
-      )
-      .then((response) => {
-        setWeather(response.data.wind);
-      });
-  }, []);
-  console.log(weather);
-
   return (
     <div className="App">
       <div>
--- src/Components/Swiper/NasSwiper.js
+++ src/Components/Swiper/NasSwiper.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import Conditions from "../Conditions/Conditions";
@@ -8,50 +8,80 @@ import { useMediaQuery } from "react-responsive";
 import "./NasSwiper.scss";
 import WeatherIcon from "../WeatherIcon/WeatherIcon";
 import sunny from "../WeatherIcon/sunny.png";
+import axios from "axios";
 
-const cities = [
+const initialValues = [
   {
     location: "Split, Croatia",
-    currentTemperature: "21",
-    currentWinds: "Light Breeze",
-    currentClouds: "Clear Sky",
-    timeAndDay: "Friday, 11am",
+    lon: 16.440193,
+    lat: 43.508133,
   },
   {
     location: "Šibenik, Croatia",
-    currentTemperature: "21",
-    currentWinds: "Light Breeze",
-    currentClouds: "Clear Sky",
-    timeAndDay: "Friday, 11am",
+    lon: 15.8942,
+    lat: 43.7343,
   },
   {
     location: "Zagreb, Croatia",
-    currentTemperature: "21",
-    currentWinds: "Light Breeze",
-    currentClouds: "Clear Sky",
-    timeAndDay: "Friday, 11am",
+    lon: 15.8942,
+    lat: 43.7343,
   },
   {
     location: "Zadar, Croatia",
-    currentTemperature: "21",
-    currentWinds: "Light Breeze",
-    currentClouds: "Clear Sky",
-    timeAndDay: "Friday, 11am",
+    lon: 15.8942,
+    lat: 43.7343,
   },
   {
     location: "Osijek, Croatia",
-    currentTemperature: "21",
-    currentWinds: "Light Breeze",
-    currentClouds: "Clear Sky",
-    timeAndDay: "Friday, 11am",
+    lon: 15.8942,
+    lat: 43.7343,
   },
 ];
 
 const NasSwiper = () => {
+  const [weather, setWeather] = useState("");
+
+  const [cities, setCities] = useState([]);
+
+  useEffect(() => {
+    // const citiesCopy = [...cities];
+
+    Promise.all(
+      initialValues.map((city) =>
+        axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&units=metric&appid=b734bae5a310113c213110ccdbf8a446`
+        )
+      )
+    ).then((values) => {
+      console.log(values);
+      setCities(
+        values.map((value) => {
+          return {
+            location: value.data.name,
+          };
+        })
+      );
+    });
+
+    // .then((response) => {
+    //   const updatedCity = {
+    //     ...city,
+    //     currentTemperature: response.data.main.temp,
+    //   };
+
+    //   citiesCopy[i] = updatedCity;
+    //   setCities(citiesCopy);
+    //   i++;
+    //   console.log(i, cities);
+    // });
+  }, []);
+  /*   console.log(cities);
+   */
   const isDesktopOrLaptop = useMediaQuery({
     query: "(min-width: 1024px)",
   });
 
+  console.log(cities);
   return (
     <div className="Swiper">
       <Swiper
