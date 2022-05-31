f80d6fb6181ed88597c4a9539e82d2a49867b31f
Luka <luka@locastic.com>
2022-06-01 01:07:10 +0200

    partial rework

--- src/Pages/Overview.js
+++ src/Pages/Overview.js
@@ -2,17 +2,16 @@ import React from "react";
 import Settings from "../Components/Settings/Settings";
 import NasSwiper from "../Components/Swiper/NasSwiper";
 import NasNavbar from "../Components/Navbar/Navbar";
-import ViewStats from "../Components/ViewStats/ViewStats";
 import "./Overview.scss";
 
 const Overview = () => {
-  return (
-    <div className="overview">
-      <Settings />
-      <NasSwiper />
-      <NasNavbar />
-    </div>
-  );
+	return (
+		<div className="overview">
+			<Settings />
+			<NasSwiper />
+			<NasNavbar />
+		</div>
+	);
 };
 
 export default Overview;
